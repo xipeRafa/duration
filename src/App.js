d8ad9d8fa5e247fd21fb56151fbaddd32d88238a
rafael <rafaelsistem87@gmail.com>
2022-01-09 00:56:15 -0700

    ff

--- src/App.js
+++ src/App.js
@@ -8,39 +8,90 @@ registerLocale("es", es);
 
 const App = ()=> {
 
-const [fecha, setFecha]=useState()
+  const [fecha, setFecha]=useState()
 
-const onChange=fecha=>{
-  setFecha(fecha);
-  console.log(fecha)
-  /* console.log('parse:', Date.parse(fecha)) parse te convierte con horas */
+  const [hoy, setHoy]=useState()
+  const [ma, setMa]=useState()
 
-  let today = fecha.getTime() //convierte la fecha en microsegundos
-  let tomorrow = today + 86400000
-  console.log(today)
-  console.log(tomorrow)
-}  
+  const onChange=fecha=>{
+    setFecha(fecha);
 
-const mostrarFecha = fecha=>{
-  const options = {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'};
-  console.log(fecha.toLocaleDateString('es-ES', options));
-}
+    /* console.log('parse:', Date.parse(fecha)) parse te convierte con horas */
 
-    return (
-      <>
-        <div className="contenedor">
-          <div className="center">
-           <DatePicker  selected={fecha} onChange={onChange} locale="es" className="pickers" 
+    let today = fecha.getTime() //convierte la fecha en microsegundos
+    let tomorrow = today + 86400000
+
+    setHoy(today)
+    setMa(tomorrow)
+  }  
+
+  const [duration, setDuration]=useState()
+
+  const [sec, setSec]=useState()
+
+  const handlerDuration = (e) =>{
+    setDuration(e.target.value)
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault()
+    setSec(duration)
+  }
+
+
+  let date = new Date(sec * 1).toLocaleDateString("es-CL", {
+    weekday: "long", // narrow, short
+    year: "numeric", // 2-digit
+    month: "long", // numeric, 2-digit, narrow, long
+    day: "numeric", // 2-digit
+  })
+
+  const time = new Date(sec * 1).toLocaleTimeString("es-CL") 
+
+
+
+  const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+
+  let fechaa = fecha?.toLocaleDateString('es-ES', options)
+
+  const hora = new Date(hoy).toLocaleTimeString("es-CL")
+
+
+  return (
+      
+        <div className="container bg-dark  py-5 m-5 row">
+
+          <p className="text-white m-0">86 400 000</p>
+
+          <div className="my-5 col-md-3">
+           <DatePicker  selected={fecha} onChange={onChange} locale="es" className="pickers form-control" 
                         dateFormat="dd 'de' MMMM 'de' yyyy"/>
-           <br /><br />
-           
-           <input type="button" value="Mostrar Fecha" className="btn btn-primary" 
-                  onClick={()=>mostrarFecha(fecha)}/>
-
           </div>
+
+          <div className={fecha !== undefined ? 'text-white' : 'd-none'}>
+            <p className="m-0 mb-1">
+              {fechaa}: {hoy} <span className="text-dark"> ---- </span>hora: {hora}
+            </p>
+            <p className="m-0">
+              <span className="text-dark"> ------------------ </span> un dia mas: {ma}
+              <span className="text-dark"> ----</span>hora: {hora}
+            </p>
+          </div>
+
+            <br/>
+          <form onSubmit={onSubmit} className="col-md-3 my-5">
+            <input className="form-control" type="text" onChange={handlerDuration}/>
+          </form>
+
+          <div>
+            <p className={sec > 0 ? 'text-white' : 'd-none'}>  
+              {date} <span className="text-dark"> ---- </span>hora: {time} 
+            </p>
+          </div>
+
         </div>
-      </>
-    );
+      
+  );
 
 }
 
