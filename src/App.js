02b81a32ec5d75ac2ca3a403952977a52ebfd46c
fer <fer@fers-iMac.local>
2024-08-09 15:47:19 -0700

    koko

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ registerLocale("es", es);
 
 const App = ()=> {
 
+   // const formateador = new Intl.DateTimeFormat("es-MX", {
+   //      dateStyle: "long",
+   //      timeStyle: "short",
+   //  });
+
+   //  const milisegundosComoFecha = (milisegundos) => {
+   //      return formateador.format(new Date(milisegundos));
+   //  };
+
+
+
+  
+
+
 
   const [ hoy, setHoy ]=useState() // miliseconds 
   const [fecha, setFecha]=useState() //Fri Feb 03 2023 00:00:00 GMT-0700 (hora estándar del Pacífico de México)
@@ -42,34 +56,34 @@ const App = ()=> {
       
         <div className="container bg-dark  py-5 m-5 row">
 
-          <p className="text-white m-0">Select the Day</p>
-          {/* <p>86 400 000 let today00 = Date.parse(new Date().toDateString())</p> */}
+            <p className="text-white m-0">Select the Day 86400000</p>
+         
 
-          <div className="my-5 col-md-3">
-              <DatePicker 
-                selected={fecha}
-                onChange={onChangeDatePicker} 
-                locale="es" 
-                className="pickers form-control" 
-                dateFormat="dd 'de' MMMM 'de' yyyy"
+            <div className="my-5 col-md-3">
+                <DatePicker 
+                    selected={fecha}
+                    onChange={onChangeDatePicker} 
+                    locale="es" 
+                    className="pickers form-control" 
+                    dateFormat="dd 'de' MMMM 'de' yyyy"
+                />
+            </div>
+
+            <p className={fecha !== undefined ? 'text-white' : 'd-none'}> {fechaDate} hora: {hora} </p>              
+
+            <div className="my-5 col-md-3">
+              <input 
+                    className="form-control mb-1" 
+                    type="text" 
+                    onChange={handlerDuration} 
+                    value={hoy} 
+                    placeholder='miliseconds'
               />
-          </div>
-
-          <p className={fecha !== undefined ? 'text-white' : 'd-none'}> {fechaDate} hora: {hora} </p>              
-
-          <div className="my-5 col-md-3">
-            <input 
-                className="form-control mb-1" 
-                type="text" 
-                onChange={handlerDuration} 
-                value={hoy} 
-                placeholder='miliseconds'
-            />
-          </div>
+            </div>
 
 
           <p className={hoy > 0 ? 'text-white' : 'd-none'}> 
-              un dia siguiente es {Number(hoy) + 86400000}{' '} La hora es {hora} 
+               {date}-----{hora} 
           </p>
 
         </div>
